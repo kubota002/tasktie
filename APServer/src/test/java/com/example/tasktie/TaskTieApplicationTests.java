7065fd755eb4324ecd4b90cdf2393e1ee97aff8d
kohei <kohei.kubota002@gmail.com>
2024-03-02 17:55:38 +0900

    first commit

---
+++ APServer/src/test/java/com/example/tasktie/TaskTieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tasktie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
